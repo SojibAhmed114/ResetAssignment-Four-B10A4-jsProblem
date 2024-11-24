ac390714564b61317f30df2f66c64807eb30cc48
SojibAhmed114 <sojib.ahmed7620@gmail.com>
2024-11-24 11:20:51 +0600

    problem one and two done

--- jsProblem.js
+++ jsProblem.js
@@ -1,1 +1,31 @@
 //problem 01
+function calculateTax(income, expenses) {
+
+    if (income < 0 || expenses < 0 || income < expenses) {
+        return "Invalid Input";
+    }
+
+    const totalIncome = income - expenses;
+    const tax = totalIncome * 0.20;
+    return tax;
+}
+
+
+
+
+// problem 02 
+function sendNotification(email) {
+    if (!email.includes("@")) {
+        return "Invalid Email"
+    }
+    const emailName = email.split('@');
+    const  name = emailName[0];
+    const domainName = emailName[1];
+    const result = name + " sent you an email from "+ domainName;
+    return result;
+}
+// console.log(sendNotification('zihad@gmail.com'));
+// console.log(sendNotification("farhan34@yahoo.com"));
+// console.log(sendNotification('nadim.naem5@outlook.com'));
+// console.log(sendNotification("fahim234.hotmail.com"));
+// console.log(sendNotification("sadia8icloud.com"));
