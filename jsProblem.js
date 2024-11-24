96098e8434cd01335004c22eafd45735c13c4d58
SojibAhmed114 <sojib.ahmed7620@gmail.com>
2024-11-24 16:32:29 +0600

    problem 4 done but problem 3 pari nai

--- jsProblem.js
+++ jsProblem.js
@@ -16,7 +16,7 @@ function calculateTax(income, expenses) {
 // problem 02 
 function sendNotification(email) {
     if (!email.includes("@")) {
-        return "Invalid Email"
+        return "Invalid Email";
     }
     const emailName = email.split('@');
     const  name = emailName[0];
@@ -24,8 +24,51 @@ function sendNotification(email) {
     const result = name + " sent you an email from "+ domainName;
     return result;
 }
-// console.log(sendNotification('zihad@gmail.com'));
-// console.log(sendNotification("farhan34@yahoo.com"));
-// console.log(sendNotification('nadim.naem5@outlook.com'));
-// console.log(sendNotification("fahim234.hotmail.com"));
-// console.log(sendNotification("sadia8icloud.com"));
+
+
+// problem 03  pari nai
+function checkDigitsInName(name) {
+    if (typeof name !== "string") {
+        return "Invalid Input";
+    }
+    
+    let demo = 0;
+    for (const yours of name){
+        if (typeof yours === 'string'){
+            demo.push(yours);
+            
+        }
+        return false;
+    }
+
+    return demo;
+    
+}
+// let sdf = checkDigitsInName(Raj123)
+// console.log(sdf);
+// console.log(checkDigitsInName("n9ayeem"));
+// console.log(checkDigitsInName('e1mu3'));
+// console.log(checkDigitsInName("Suman"));
+// console.log(checkDigitsInName("Name2024"));
+// console.log(checkDigitsInName("!@#"));
+// console.log(checkDigitsInName(["Raj"]));
+
+
+
+// problem 04
+function calculateFinalScore(obj) {
+    if (typeof obj !== 'object' || Array.isArray(obj)) {
+        return "Invalid Input";
+    }
+
+    if (obj.isFFamily) {
+        obj.isFFamily = 20;
+    }
+
+    if (obj.testScore <= 50 &&  obj.schoolGrade <= 30) {
+        const marks = obj.testScore + obj.schoolGrade + obj.isFFamily;
+        return marks >= 80? true : false;
+    }
+
+}
+
